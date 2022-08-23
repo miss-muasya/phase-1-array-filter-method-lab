317db618210715d5e8bf153b2e17b9aa29f2e6ba
miss-muasya <sheemuasya@gmail.com>
2022-08-23 16:36:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, taxi) {
+    return drivers.filter((possibleMatch) => possibleMatch.toLowerCase() === taxi.toLowerCase());
+    }
+function  fuzzyMatch(drivers, oneTaxi) {
+        return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().startsWith(oneTaxi.toLowerCase()));
+    }
+function matchName(drivers, matchName) {
+        return drivers.filter((record) => record.name === matchName);
+        
+    }
